accc167f4965965e7a2891d8c544f410a247a230
000hen <git@muisnowdevs.one>
2025-10-30 12:41:07 +0800

    [refactor] remove unused WebhookUrlPayload from AppEventMap

--- app/.server/queue/tasks/ProcessData.ts
+++ app/.server/queue/tasks/ProcessData.ts
@@ -3,26 +3,18 @@ export enum QueueNames {
     CLIENT_EVENTS = "clientEvents",
 }
 
-interface WebhookUrlPayload {
-    url: string;
-    clientId: string;
-}
-
 export interface AppEventMap {
     "uesr.updated": {
         userId: string;
         changes: Record<string, any>;
-        sending?: WebhookUrlPayload;
     };
     "user.deleted": {
         userId: string;
         clients: string[];
-        sending?: WebhookUrlPayload;
     };
     "user.revoked": {
         userId: string;
         clientId: string;
-        sending?: WebhookUrlPayload;
     };
     "email.sent": { to: string; subject: string; body: string };
     "webhook.sent": {
